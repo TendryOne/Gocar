5b7541a1c87635ce861e8631b12ce2e02b12ad05
TendryOne <Tendryone@hotmail.com>
2023-08-17 23:08:40 +0300

    Mise en place de systeme de filtre

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {AppNavigator} from './Navigation';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your !</Text>
-      <StatusBar style="auto" />
+    <View style={{flex : 1 , backgroundColor : 'white'}}>
+          <NavigationContainer>
+        <AppNavigator/>
+        </NavigationContainer>
     </View>
   );
 }
